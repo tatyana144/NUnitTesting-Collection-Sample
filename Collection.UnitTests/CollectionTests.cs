5f067e62691505752b85121cc4abb66ec94bf9f1
TatyanaAn <tatyana.andreeva@pfgbulgaria.com>
2023-02-06 22:02:49 +0200

    Add project files.

---
+++ Collection.UnitTests/CollectionTests.cs
@@ -0,0 +1,13 @@
+namespace Collection.UnitTests
+{
+    public class CollectionTests
+    {
+       
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
